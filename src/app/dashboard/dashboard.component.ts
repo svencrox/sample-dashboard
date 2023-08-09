b8980e1350815ab0fd91da3a07d8d279c56255b4
Wilson <141197657+wilsonxamble@users.noreply.github.com>
2023-08-09 15:02:56 +0800

    (update and refactor): Fix issue with sidebar not behaving properly,refactor indentation and code with better syntax

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from "@angular/core";
+import { Chart } from "chart.js/auto";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+	selector: "app-dashboard",
+	templateUrl: "./dashboard.component.html",
+	styleUrls: ["./dashboard.component.scss"],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+	amountChart: any;
+	ngOnInit(): void {
+		this.initLineChart();
+	}
 
+	@HostListener("window:resize", ["$event"])
+	onWindowResize(event: Event): void {
+		for (let id in Chart.instances) {
+			Chart.instances[id].resize();
+		}
+	}
+
+	initLineChart() {
+		const amountChartId = document.getElementById("amount-payable") as HTMLCanvasElement;
+		this.amountChart = new Chart(amountChartId, {
+			type: "line",
+			data: {
+				labels: ["January", "February", "March", "April", "May"],
+				datasets: [
+					{
+						label: "Amount Payable",
+						data: [150, 230, 450, 300, 550],
+						tension: 0.1,
+					},
+				],
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: true,
+			},
+		});
+	}
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,33 +1,32 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
+	selector: "app-sidebar",
+	templateUrl: "./sidebar.component.html",
+	styleUrls: ["./sidebar.component.scss"],
 })
-export class SidebarComponent {
-  isSidebarOpen = false;
+export class SidebarComponent implements OnInit {
+	isSidebarOpen: boolean | undefined;
+	ngOnInit(): void {
+		this.checkScreenSize();
+	}
 
-  toggleSidebar(): void {
-    this.isSidebarOpen = !this.isSidebarOpen;
-  }
+	toggleSidebar(): void {
+		this.isSidebarOpen = !this.isSidebarOpen;
+	}
 
-  closeSidebar(): void {
-    if (window.innerWidth < 768) {
-      this.isSidebarOpen = false;
-    }
-  }
+	closeSidebar(): void {
+		if (window.innerWidth < 768) {
+			this.isSidebarOpen = false;
+		}
+	}
 
-  @HostListener('window:resize', ['$event'])
-  onWindowResize(event: Event): void {
-    this.checkScreenSize();
-  }
+	@HostListener("window:resize", ["$event"])
+	onWindowResize(event: Event): void {
+		this.checkScreenSize();
+	}
 
-  checkScreenSize(): void {
-    this.isSidebarOpen = window.innerWidth >= 768;
-  }
-
-  logout(): void {
-    console.log('User logged out');
-  }
+	checkScreenSize(): void {
+		window.innerWidth >= 768 ? (this.isSidebarOpen = true) : (this.isSidebarOpen = false);
+	}
 }
