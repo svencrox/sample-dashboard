9a496b9545ba4ec9ac69eae13dcce90619ff4deb
Wilson <141197657+wilsonxamble@users.noreply.github.com>
2023-08-10 17:45:41 +0800

    (Update): Install ngx-bootstrap and angular material, general spacing for code, update for app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ClientsComponent } from './clients/clients.component';
-import { UsersComponent } from './users/users.component';
-import { CampaignsComponent } from './campaigns/campaigns.component';
-import { FormsModule } from '@angular/forms';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ClientsComponent } from "./clients/clients.component";
+import { UsersComponent } from "./users/users.component";
+import { CampaignsComponent } from "./campaigns/campaigns.component";
+import { FormsModule } from "@angular/forms";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { CdkAccordionModule } from "@angular/cdk/accordion";
+import { ModalModule } from "ngx-bootstrap/modal";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { ClientFormModalComponent } from "./client-form-modal/client-form-modal.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    ClientsComponent,
-    UsersComponent,
-    CampaignsComponent,
-    SidebarComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent, DashboardComponent, ClientsComponent, UsersComponent, CampaignsComponent, SidebarComponent, ClientFormModalComponent],
+	imports: [BrowserModule, MatIconModule, MatButtonModule, AppRoutingModule, FormsModule, MatExpansionModule, CdkAccordionModule, BrowserAnimationsModule, ModalModule.forRoot()],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
